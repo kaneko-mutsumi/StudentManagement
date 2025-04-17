aef1c630320e0d7cf5bf382b2a1ad4e3b736c839
kaneko-mutsumi <oto.010wz@gmail.com>
2025-04-18 01:41:01 +0900

    課題24：年代やコース名で任意に絞り込みできる機能を追加

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -2,34 +2,22 @@ package raisetech.StudentManagement.repository;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.date.Student;
 import raisetech.StudentManagement.date.StudentsCourses;
 
 
-/**
- * 受講生情報を扱うリポジトリ。
- *
- * 全体検索や単一条件での検索、コース情報の検索が行えるクラスです。
- *
- */
-
-
 @Mapper
 public interface StudentRepository {
 
-  /**
-   *
-   * 全権検索します。
-   *
-   * @return　全件検索した受講生情報の一覧
-   */
 
+ // 年齢の範囲で生徒を検索
+  @Select("SELECT * FROM students WHERE age BETWEEN #{from} AND #{to}")
+  List<Student> findStudentsByAgeRange(@Param("from") int from, @Param("to") int to);
 
-  @Select("SELECT * FROM students")
-  List<Student> search();
-
-  @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCourses();
+  // コース名で受講生を検索
+  @Select("SELECT * FROM students_courses WHERE course_name = #{courseName}")
+  List<StudentsCourses> findCoursesByName(@Param("courseName") String courseName);
 
 }
