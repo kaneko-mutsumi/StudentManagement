7fb0a3893dc952a723c6e031958193e707389c38
kaneko-mutsumi <oto.010wz@gmail.com>
2025-04-18 00:29:01 +0900

    課題24：30代・Javaコースで絞り込みするフィルター処理を追加

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.date.Student;
 import raisetech.StudentManagement.date.StudentsCourses;
@@ -16,14 +17,19 @@ public class StudentController {
 
   private final StudentService service;
 
-@GetMapping("/studentList")
-  public List<Student> getStudentList(){
-  return service.searchStudentList();
-}
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return service.searchStudentList();
+  }
 
   @GetMapping("/studentsCoursesList")
   public List<StudentsCourses> getStudentsCoursesList() {
     return service.searchStudentsCoursesList();
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudentsByAge(@RequestParam("age") int age){
+    return service.searchStudentsByAge(age);
   }
 
 }
