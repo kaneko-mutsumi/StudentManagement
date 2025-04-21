4c225c1df5da7972f63cc621af45d899839510e8
kaneko-mutsumi <oto.010wz@gmail.com>
2025-04-21 23:08:41 +0900

    現在の課題24の実装で使用していない `StudentSearchCondition.java` を削除しました。

--- src/main/java/raisetech/StudentManagement/request/StudentSearchCondition.java
+++
@@ -1,9 +0,0 @@
-package raisetech.StudentManagement.model;
-
-import lombok.Data;
-
-@Data
-public class StudentSearchCondition {
-  private Integer age;         // 10, 20, 30などの年代
-  private String courseName;   // Javaコースなど
-}
